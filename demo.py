3e432da3000ec309f4853837667dd253e4ce8ba4
Nan Wang <wangnangg@gmail.com>
2018-07-09 21:08:16 -0400

    revise comment

--- demo.py
+++ demo.py
@@ -12,11 +12,11 @@ butools.verbose = True
 # generate 10000 samples from a Pareto distribution
 tr = pareto.rvs(3, size = 10000)
 
-# draw the empirical CDF of the samples
+# plot the empirical CDF of the samples
 (xt, yt) = btrace.CdfFromTrace(tr)
 plt.plot(xt, yt, label='Trace')
 
-# trying fitting using 3, 5, and 7 states
+# try fitting using 3, 5, and 7 states
 nstates = [3, 5, 7]
 for ns in nstates:
 
@@ -27,7 +27,7 @@ for ns in nstates:
     # alpha: initial probability of the Markov chain
     # A: transition rate matrix between transient states
 
-    # draw the fitted CDF
+    # plot the fitted CDF
     intBounds = np.linspace(0, 20, 1000)
     yf = bph.CdfFromPH(alpha, A, intBounds)
     plt.plot(intBounds, yf, label='Fit with ' + str(ns) + ' transient states.' )
